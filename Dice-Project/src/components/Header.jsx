d5cba57ae4ffcb0932cc5cfe52d41443173a63f3
ddewaay <ddewaay@uiowa.edu>
2024-02-04 04:35:17 -0600

    Send it

---
+++ Dice-Project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <nav>
+      <h1 class="menu">Polymer</h1>
+      <div>
+        <h1 class="menu">🌑</h1>
+      </div>
+    </nav>
+  );
+}
---
+++ Dice-Project/src/components/Main.jsx
@@ -0,0 +1,9 @@
+import { Canvas } from "@react-three/fiber";
+
+export default function Main() {
+  return (
+    <div className="frame">
+      <Canvas></Canvas>
+    </div>
+  );
+}
