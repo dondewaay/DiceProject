33082d8bd44e9f787c5c3031af2cee998476cf99
ddewaay <ddewaay@uiowa.edu>
2024-02-04 15:48:43 -0600

    Small change

--- Dice-Project/src/components/Model.jsx
+++ Dice-Project/src/components/Model.jsx
@@ -1,6 +1,6 @@
 export default function Model() {
   return (
-    <mesh position={[1, 1, 1]}>
+    <mesh position={[1.5, 0, 2]}>
     <boxGeometry args={[1, 1, 1]} />
     <meshStandardMaterial color={"grey"} />
   </mesh>
