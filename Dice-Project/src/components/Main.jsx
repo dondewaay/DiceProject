8280c26e79b8d2f8e5a5ccf9d42ddb88b853f4c9
ddewaay <ddewaay@uiowa.edu>
2024-02-04 15:34:13 -0600

    Huge progress!

--- Dice-Project/src/components/Main.jsx
+++ Dice-Project/src/components/Main.jsx
@@ -1,14 +1,18 @@
 import Button from "./Button";
+import Model from "./Model";
 import { Canvas } from "@react-three/fiber";
 
 export default function Main() {
   return (
-    <Canvas className="canvas">
-      <directionalLight position={[0, 0, 5]} />
-      <mesh position={[1, 1, 1]}>
-        <boxGeometry args={[4, 4, 4]} />
-        <meshStandardMaterial color={"grey"} />
-      </mesh>
-    </Canvas>
+    <div className="canvas">
+      <Button />
+      <Canvas>
+        <directionalLight position={[0, 0, 5]} />
+        <mesh position={[1, 1, 1]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color={"grey"} />
+        </mesh>
+      </Canvas>
+      </div>
   );
 }
--- Dice-Project/src/components/Model.jsx
+++ Dice-Project/src/components/Model.jsx
@@ -1,2 +1,8 @@
 export default function Model() {
+  return (
+    <mesh position={[1, 1, 1]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={"grey"} />
+    </mesh>
+  );
 }
