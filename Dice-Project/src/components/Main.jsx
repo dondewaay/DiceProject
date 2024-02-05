dc1632a2e9da0b0323adef72c0db6b46cab28876
ddewaay <ddewaay@uiowa.edu>
2024-02-05 16:56:26 -0600

    Big

--- Dice-Project/src/components/Main.jsx
+++ Dice-Project/src/components/Main.jsx
@@ -6,10 +6,12 @@ import "../styles/Home.css";
 export default function Main() {
   return (
     <div className="canvas">
-      <Canvas>
-        <directionalLight position={[2, 2, 2]} />
-        <Model position={[1.2, 0, 1]} size={2} detail={0} color={"grey"} />
-      </Canvas>
+      <div className="threed">
+        <Canvas>
+          <directionalLight position={[2, 2, 2]} />
+          <Model position={[0, 0, 0]} size={2.5} detail={0} color={"grey"} />
+        </Canvas>
+      </div>
       <div className="billboard">
         <h1>
           Polymer? <br />I hardl-
