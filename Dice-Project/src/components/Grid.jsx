b26425cafe99f7b31ce19fa3aa510575a64d47b2
ddewaay <dondewaay@gmail.com>
2024-02-14 16:55:46 -0600

    Couple fixes

--- Dice-Project/src/components/Grid.jsx
+++ Dice-Project/src/components/Grid.jsx
@@ -3,14 +3,16 @@ import NewCard from "./NewCard";
 
 export default function Grid() {
   return (
-    <div className="grid">
-      <Card name="Potion" />
-      <Card name="Claymore" />
-      <Card name="Saving Throw" />
-      <Card name="Initiave" />
-      <Card name="Custom" />
-      <Card name="Custom" />
-      <NewCard />
+    <div className="grid-container">
+      <div className="grid">
+        <Card name="Potion" />
+        <Card name="Claymore" />
+        <Card name="Saving Throw" />
+        <Card name="Initiave" />
+        <Card name="Custom" />
+        <Card name="Custom" />
+        <NewCard />
+      </div>
     </div>
   );
 }
