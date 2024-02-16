1ff0f73cf02d410e8eeadfac5ba627db1a2f7cc8
ddewaay <ddewaay@uiowa.edu>
2024-02-16 16:30:31 -0600

    JS logic start

---
+++ Dice-Project/src/components/diceCards.jsx
@@ -0,0 +1,19 @@
+export default function diceCards(
+  d2die,
+  d4die,
+  d6die,
+  d8die,
+  d10die,
+  d12die,
+  d20die,
+  d100die
+) {
+  (this.d2 = d2die),
+    (this.d4 = d4die),
+    (this.d6 = d6die),
+    (this.d8 = d8die),
+    (this.d10 = d10die),
+    (this.d12 = d12die),
+    (this.d20 = d20die),
+    (this.d100 = d100die);
+}
